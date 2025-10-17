5c0c3b47bd015d58609acd10ebd8b8db5714023c
sangbaelim-db <89111719+sangbaelim-db@users.noreply.github.com>
2025-10-17 13:46:35 +0900

    Update test1.sql

--- sqltest/test1.sql
+++ sqltest/test1.sql
@@ -1,1 +1,1 @@
-select *, ':var_date' as param from slimdemo.slimtest.bronze where data(updated_at) = date(:var_date);
+select *, ':var_date' as param from slimdemo.slimtest.bronze where date(updated_at) = date(:var_date);
